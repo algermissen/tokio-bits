0f3a0771270edef0bf2fcc2da24b6d41e6566fad
Jan Algermissen <algermissen@acm.org>
2017-12-29 16:06:15 +0100

    Make code more general

--- tokio-process-bits/src/lib.rs
+++ tokio-process-bits/src/lib.rs
@@ -5,60 +5,57 @@ extern crate tokio_process;
 extern crate futures;
 extern crate tokio_core;
 
-//use tokio_core::reactor::Core;
 use tokio_core::reactor::Handle;
 use tokio_process::CommandExt;
 use tokio_io::io::lines;
 use futures::Stream;
-use std::io;
+use std::io::{BufReader, ErrorKind, Error};
 use std::process::Stdio;
 
+/// Hdl is a handler type combining the child future and
+/// stdout and stderr streams returned from the execute
+/// function.
+pub struct Hdl {
+    /// The child future. If this goes out of scope, the process will be killed.
+    /// Use the forget() method to avoid that.
+    pub child: ::tokio_process::Child,
+    /// Line based stream of child process' stdout
+    pub stdout: Box<Stream<Item = String, Error = Error>>,
+    /// Line based stream of child process' stderr
+    pub stderr: Box<Stream<Item = String, Error = Error>>,
+}
+
 // Create a new command from the given command and arguments and
-// spawn a child that runs the given command and return a line-by-line
-// stream of its stdout. Stderr is ignored.
-pub fn cmd_stdout<'a, I>(
-    handle: &Handle,
-    c: &str,
-    args: I,
-) -> Box<Stream<Item = String, Error = io::Error>>
+// spawn a child that runs the given command and returns line-by-line
+// streams of its stdout and stderr.
+pub fn execute<'a, I>(handle: &Handle, c: &str, args: I) -> Result<Hdl, Error>
 where
     I: IntoIterator<Item = &'a str>,
 {
     let mut cmd = ::std::process::Command::new(c);
     cmd.args(args);
 
-    // Let us read stdout and ignore stderr
+    // We want to read stderr and stdout
     cmd.stdout(Stdio::piped());
-    cmd.stderr(Stdio::null());
+    cmd.stderr(Stdio::piped());
 
-    // Spawn the child
-    let mut child = cmd.spawn_async(handle).expect("spawning child to succeed");
-    let id = child.id();
+    // Spawn the child process
+    let mut child = try!(cmd.spawn_async(handle));
 
-    // Create a line-based stream from stdout of child
-    let stdout = child.stdout().take().expect("to get stdout handle");
-    let reader = ::std::io::BufReader::new(stdout);
-    let stream = lines(reader).map(move |line| format!("[CHILD {}] {}", id, line));
+    // Create line-based streams from stdout and stderr of child
+    let stdout_stream = match child.stdout().take() {
+        Some(reader) => Box::new(lines(BufReader::new(reader))),
+        None => return Result::Err(Error::new(ErrorKind::Other, "stdout ws not captured")),
+    };
+    let stderr_stream = match child.stderr().take() {
+        Some(reader) => Box::new(lines(BufReader::new(reader))),
+        None => return Result::Err(Error::new(ErrorKind::Other, "stderr ws not captured")),
+    };
 
-    // Make sure the child process survives the call to the 'child' variable destructor
-    // If we did not call this, child process would be killed if cmd_stdout(..) returns.
-    child.forget();
-
-    Box::new(stream)
+    let h = Hdl {
+        child: child,
+        stdout: stdout_stream,
+        stderr: stderr_stream,
+    };
+    Result::Ok(h)
 }
-
-
-//fn main() {
-//    let mut core = Core::new().unwrap();
-//    let handle = core.handle();
-//
-//    let s1 = cmd_stdout(&handle, "ping", vec!["127.0.0.1"]);
-//    let s2 = cmd_stdout(&handle, "ping", vec!["0.0.0.0"]);
-//
-//    let h = s1.select(s2).for_each(|line| {
-//        println!("LINE: {}", line);
-//        ::futures::future::ok(())
-//    });
-//
-//    core.run(h).unwrap();
-//}
